255761e617f310c738e8726f2723cc83fa8e13fe
Edgar <ed.galstyan01@gmail.com>
2021-09-28 00:40:22 +0400

    Working on scheduler

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 import 'devextreme/dist/css/dx.common.css';
+import 'devextreme/dist/css/dx.light.css';
+import 'devextreme/dist/css/dx.common.css';
 import './themes/generated/theme.base.css';
 import './themes/generated/theme.additional.css';
 
@@ -10,6 +12,7 @@ import appInfo from "./app-info";
 
 const headers = {
     'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*',
     'Authorization': `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVfbmFtZSI6WyJhZG1pbiIsIkN1c3RvbU9iamVjdENhbkJlQWRkZWRIZXJlIl0sIm5iZiI6MTYzMjM3NTg1NywiZXhwIjoxNjMyOTgwNjU3LCJpYXQiOjE2MzIzNzU4NTd9.8ahLrsIzCEHW0EHk59dcG4Y3lF2lrao718QJ4pyQ2UY`
 };
 
--- src/views/scheduler.vue
+++ src/views/scheduler.vue
@@ -1,117 +1,143 @@
 <template>
-    <div class="schedule-vue-sample">
-        <div class="col-md-9 control-section">
-            <div class="content-wrapper" >
-                <ejs-schedule id='Schedule' height="650px" :selectedDate='selectedDate' :eventSettings='eventSettings'></ejs-schedule>
-            </div>
-        </div>
-        <!-- <div class="col-lg-3 property-section">
-            <table id="property" title="Properties" style="width: 100%">
-                <tbody>
-                    <tr style="height: 50px">
-                        <td style="width: 30%">
-                            <div>Current Date</div>
-                        </td>
-                        <td style="width: 70%;">
-                            <div>
-                                <ejs-datepicker id='datepicker' :value='selectedDate' :showClearButton='false' :change='onDateChange'></ejs-datepicker>
-                            </div>
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
-        </div> -->
+  <div class="schedule-vue-sample">
+    <div class="col-md-9 control-section">
+      <div class="content-wrapper" id="scheduler">
+        <DxScheduler
+          text-expr="title"
+          all-day-expr="dayLong"
+          :views="views"
+          :data-source="appointments"
+          :show-all-day-panel="false"
+          :groups="groups"
+          :current-view="currentView"
+          recurrence-rule-expr="recurrence"
+          :current-date="currentDate"
+          :customize-date-navigator-text="customizeDateNavigatorText"
+        >
+          <!-- <DxView
+            :cell-duration="30"
+            :interval-count="1"
+            type="workWeek"
+            name="Vertical Grouping"
+            group-orientation="vertical"
+          /> -->
+          <DxResource
+            :allow-multiple="false"
+            :data-source="priorityData"
+            field-expr="priorityId"
+            label="Priority"
+          />
+          <template #dateCellTemplate="{ data }">
+            <b v-bind:style="{ color: 'blue' }">{{ data.text }}</b>
+          </template>
+          <template #timeCellTemplate="{ data }">
+            <b
+              v-bind:style="{ color: 'green', height: '20px', fontSize: '8px' }"
+              >{{ Number(data.text.match(/^(\d+)/)[1]) }}</b
+            >
+          </template>
+        </DxScheduler>
+      </div>
     </div>
+  </div>
 </template>
 <script>
-    import Vue from "vue";
-    import { extend } from '@syncfusion/ej2-base';
-    import { SchedulePlugin, Day, Week, WorkWeek, Month, Agenda, Resize, DragAndDrop } from "@syncfusion/ej2-vue-schedule";
-    // import { DatePickerPlugin } from '@syncfusion/ej2-vue-calendars';
-    // import { DataManager, WebApiAdaptor } from '@syncfusion/ej2-data';
-    Vue.use(SchedulePlugin);
-    // Vue.use(DatePickerPlugin);
+import Vue from "vue";
+import "devextreme/dist/css/dx.light.css";
 
-  //   var dataManger = new DataManager({
-  //       url: 'https://5.189.167.236:44376/v1/api/Callstest',
-  //       adaptor: new WebApiAdaptor,
-  //       crossDomain: true,
-  //       headers:[{'Content-Type': 'application/json',
-  //   'Authorization': `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVfbmFtZSI6WyJhZG1pbiIsIkN1c3RvbU9iamVjdENhbkJlQWRkZWRIZXJlIl0sIm5iZiI6MTYzMjM3NTg1NywiZXhwIjoxNjMyOTgwNjU3LCJpYXQiOjE2MzIzNzU4NTd9.8ahLrsIzCEHW0EHk59dcG4Y3lF2lrao718QJ4pyQ2UY`
-  //       }]
-  //   });
+import { DxScheduler, DxResource } from "devextreme-vue/scheduler";
 
-  //   dataManger.ready().then((el) => {
-  //     console.log('--------', el);   
-  // });
-
-    export default Vue.extend({
-        data: function () {
-            return {
-              eventSettings: { 
-                dataSource: extend([], [], null, true)
-              },
-              selectedDate: new Date(2021, 0, 26)
-            }
+export default Vue.extend({
+  data: function() {
+    return {
+      appointments: [],
+      groups: ["priorityId"],
+      currentView: "timelineDay",
+      currentDate: new Date(2020, 12, 14),
+      views: [
+        {
+          name: "Days",
+          type: "timelineDay",
+          intervalCount: 15,
+          startDate: new Date(2020, 12, 14),
         },
-        provide: {
-            schedule: [Day, Week, WorkWeek, Month, Agenda, Resize, DragAndDrop]
+      ],
+      priorityData: [
+        {
+          text: "Berth 1",
+          id: 1,
+          color: "#1e90ff",
         },
-        mounted() {
-          this.getScheduleData();
+        {
+          text: "Berth 2",
+          id: 2,
+          color: "#ff9747",
         },
-        methods: {
-          getScheduleData() {
+        {
+          text: "Berth 3",
+          id: 3,
+          color: "#2c39f5",
+        },
+      ],
+    };
+  },
+  components: {
+    DxScheduler,
+    DxResource,
+  },
+  mounted() {
+    this.getScheduleData();
+  },
+  methods: {
+    getScheduleData() {
+      Vue.$http.get("Callstestrw").then((data) => {
+        console.log(data);
+        const sheduleData = data.data.document.records;
+        sheduleData.map((item) => {
+          const sheduleObj = {
+            title: item.vesselName,
+            startDate: new Date(item.startDT),
+            endDate: new Date(item.endDT),
+            priorityId: item.berthID,
+          };
+          this.appointments.push(sheduleObj);
+        });
+      });
+    },
 
-            let scheduleObj = document.getElementById('Schedule').ej2_instances[0];
-
-            Vue.$http.get('Callstest').then((data) => {
-              // console.log(data.data.document.records);
-              //   let Data = [{
-              //       Id: 1,
-              //       Subject: 'Conference',
-              //       StartTime: new Date(2021, 8, 26, 1, 0),
-              //       EndTime: new Date(2021, 8, 26, 2, 0),
-              //       IsAllDay: false
-              //   },{
-              //       Id: 2,
-              //       Subject: 'Meeting',
-              //       StartTime: new Date(2021, 8, 26, 3, 0),
-              //       EndTime: new Date(2021, 8, 26, 4, 0),
-              //       IsAllDay: false
-              //   }];
-              let arr = [];
-              data.data.document.records.map(obj => {
-                // let sDt = obj['startDT'].split('T');
-                // let sDt0 = sDt[0].split('-');
-                // let sDt1 = sDt[1].split(':');
-                // let eDt = obj['endDT'].split('T');
-                // let eDt0 = eDt[0].split('-');
-                // let eDt1 = eDt[1].split(':');
-
-                // console.log('---------');
-                // console.log(parseInt(sDt0[0]));
-                // console.log(parseInt(sDt0[1]) - 1);
-                // console.log(parseInt(sDt0[2]));
-                // console.log(parseInt(sDt1[0]));
-                // console.log(parseInt(sDt1[1]));
-                
-                obj['Subject'] = obj['berthName'];
-                obj['Location'] = obj['berthName'];
-                // obj['RecurrenceRule'] = 'FREQ=WEEKLY;BYDAY=MO,TU,WE,TH,FR;INTERVAL=1';
-                obj['StartTime'] = new Date(obj['startDT']);
-                obj['EndTime'] = new Date(obj['endDT']);
-                arr.push(obj); 
-              });
-console.log('-------',arr);
-              scheduleObj.addEvent(arr);
-
-            });
-          },
-          onDateChange: function (args) {
-              this.selectedDate = args.value;
-          }
-        }
-    });
-
+    customizeDateNavigatorText(e) {
+      const formatOptions = {
+        year: "numeric",
+        month: "numeric",
+        day: "numeric",
+      };
+      const formattedStartDate = e.startDate.toLocaleString(
+        "en",
+        formatOptions
+      );
+      const formattedEndDate = e.endDate.toLocaleString("en", formatOptions);
+      if ((this.currentView === "day") | "timelineDay")
+        return formattedStartDate;
+      if (this.currentView === "month")
+        return e.startDate.toLocaleString("en", {
+          year: "numeric",
+          month: "numeric",
+        });
+      return formattedStartDate + " - " + formattedEndDate;
+    },
+  },
+});
 </script>
+
+<style scoped>
+#scheduler .dx-scheduler-cell-sizes-horizontal {
+  padding: 0%;
+  margin: 0%;
+  width: 10px;
+}
+#scheduler .dx-scheduler-cell-sizes-vertical {
+  height: 100%;
+  padding: 0%;
+  margin: 0%;
+}
+</style>
